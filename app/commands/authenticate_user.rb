3edfe5a0f763cf7ccefe619cb5fcdb78e836cf32
Ethan Grab <ethangrab@gmail.com>
2020-03-27 11:39:52 -0600

    in-line comments for token based authorization/authentication implementation

--- app/commands/authenticate_user.rb
+++ app/commands/authenticate_user.rb
@@ -1,5 +1,5 @@
 class AuthenticateUser
-  prepend SimpleCommand
+  prepend SimpleCommand #allows for alias method calls
 
   def initialize(email, password)
     @email = email
@@ -15,9 +15,14 @@ class AuthenticateUser
   attr_accessor :email, :password
 
   def user
-    user = User.find_by_email(email)
+    user = User.find_by_email(email) #user lookup by email
+
+    #return the user if it exists and has correct login
+    #authenticate is a built-in method for User given by
+    #has_secure_password denotion in the model
     return user if user && user.authenticate(password)
 
+    #otherwise throw credentials error
     errors.add :user_authentication, 'invalid credentials'
     nil
   end
--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -1,10 +1,17 @@
 class JsonWebToken
  class << self
+
+#class must be included in the rails runtime config/application.rb
+
+   #takes the params User_id, key validity duration, rails unique base key)
+   #and returns a unique token
    def encode(payload, exp = 24.hours.from_now)
      payload[:exp] = exp.to_i
      JWT.encode(payload, Rails.application.secrets.secret_key_base)
    end
 
+   #takes in the token generated
+   #and uses the applications secret key to decode it
    def decode(token)
      body = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
      HashWithIndifferentAccess.new body
